f38387b1c8e1bab9a28ef6a0742cc1e9ca90c55f
ekoyudhi <ekoyudhiprastowo@gmail.com>
2022-09-14 20:48:06 +0700

    Mock Up 2

--- instruction/models.py
+++ instruction/models.py
@@ -43,15 +43,15 @@ class Group(BaseGroup):
     pass
 
 class Player(BasePlayer):
-    q1 = models.IntegerField(choices=[[1,'Nusa Jaya'],[2,'Nusa Makmur']], label='1) Anda adalah warga negara?')
-    q2 = models.IntegerField(choices=[[1,'Mencatat dan membayar konstribusi wajib'],[2,'Membayar dan melaporkan konstribusi wajib'],[3,'Mencatat dan membayar konstribusi wajib'],[4,'Mencatat, membayar, dan melaporkan konstribusi wajib']], label='2)	Kewajiban setiap warga negara Nusa Makmur yang memiliki usaha adalah?')
-    q3 = models.IntegerField(choices=[[1,'15 Periode'],[2,'12 Periode']], label='3) Anda harus melaporkan pajkonstribusi wajibak dalam berapa periode?')
+    q1 = models.IntegerField(choices=[[1,'Nusa Jaya'],[2,'Nusa Makmur']], label='1) Anda adalah warga desa?')
+    q2 = models.IntegerField(choices=[[1,'Mencatat dan membayar konstribusi wajib'],[2,'Membayar dan melaporkan konstribusi wajib'],[3,'Mencatat dan membayar konstribusi wajib'],[4,'Mencatat, membayar, dan melaporkan konstribusi wajib']], label='2)	Kewajiban setiap warga desa Nusa Makmur yang memiliki usaha adalah?')
+    q3 = models.IntegerField(choices=[[1,'15 Periode'],[2,'12 Periode']], label='3) Anda harus melaporkan kontribusi wajib dalam berapa periode?')
     q4 = models.IntegerField(choices=[[1,'Laba (Rugi)'],[2,'Omset']], label='4)	Nilai konstribusi wajib dihitung dari?(Dasar pengenaan Kontribusi Wajib)')
     q5 = models.IntegerField(choices=[[1,'Tidak ada'],[2,'Ada, dari 20% ke 10% dari Laba'],[3,'Ada, dari 10% ke 5% dari Laba'],[4,'Ada, dari 20% ke 10% dari Omset']], label='5) Apakah ada penurunan tarif, berapa nilainya?')
-    q6 = models.IntegerField(choices=[[1,'a'],[2,'b'],[3,'c'],[4,'d']], label='6) Pilih penghitungan konstribusi wajib dan pendapatan bersih yang paling tepat (Anda dapat menggunakan kalkulator)<br><img src="https://i.ibb.co/Kh4QQq1/tb1.jpg" alt="2022-02-21-195502" border="0" width="600" height="200"><br>Pilih jawaban di bawah')
+    q6 = models.IntegerField(choices=[[1,'a'],[2,'b'],[3,'c'],[4,'d']], label='6) Pilih penghitungan konstribusi wajib dan pendapatan bersih yang paling tepat (Anda dapat menggunakan kalkulator)<br><img src="https://i.ibb.co/PZxcSKW/Picture1.png" alt="2022-02-21-195502" border="0" width="600" height="200"><br>Pilih jawaban di bawah')
     q7 = models.IntegerField(choices=[[1,'Tidak diketahui'],[2,'20% s.d 40% dari total seluruh wajib konstribusi wajib Nusa Makmur'],[3,'20% s.d 40% dari total seluruh periode pelaporan konstribusi wajib'],[4,'20% s.d. 40% untuk masing-masing wajib konstribusi wajib pada setiap pelaporan konstribusi wajib-nya']], label='7) Berapa probabilitas pemeriksaan yang dilakukan oleh Nusa Makmur?')
     q8 = models.IntegerField(choices=[[1,'sebesar 100% dari selisih konstribusi wajib yang kurang dibayar'],[2,'sebesar 200% dari selisih konstribusi wajib yang kurang dibayar'],[3,'sebesar 300% dari selisih konstribusi wajib yang kurang dibayar']], label='8) Besar total denda jika terdapat konstribusi wajib yang masing kurang dibayar?')
-    q9 = models.IntegerField(choices=[[1,'a'],[2,'b'],[3,'c']], label='9)	Jika Anda menjadi petugas pemeriksa konstribusi wajib, pilih hitungan denda yang tepat (Anda dapat menggunakan kalkulator)<br><img src="https://i.ibb.co/Sm21jsh/tb2.jpg" alt="2022-02-21-195538" border="0" width="600" height="400"><br>Pilih jawaban di bawah')
+    q9 = models.IntegerField(choices=[[1,'a'],[2,'b'],[3,'c']], label='9)	Jika Anda menjadi petugas pemeriksa pemerintah desa yang melakukan pemeriksaan, pilih hitungan denda yang tepat (Anda dapat menggunakan kalkulator)<br><img src="https://i.ibb.co/xDhJrhy/Picture2.png" alt="2022-02-21-195538" border="0" width="600" height="400"><br>Pilih jawaban di bawah')
     performance = models.IntegerField(initial=0, blank=False)
     waktu = models.IntegerField(widget=widgets.RadioSelect, label='Waktu', choices=[[30,'30 detik'],[45,'45 detik'],[60,'60 detik']])
     total_omset = models.FloatField(initial=0, blank=False)
